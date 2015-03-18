f6e0f8bdc453797a8429968b7a0ca2a8241ec160
Simon Sasburg <simon.sasburg@gmail.com>
2015-03-18 18:51:27 +0100

    first try

---
+++ server.js
@@ -0,0 +1,4 @@
+require('typescript-require')({
+	nodeLib: true
+});
+require('./server.ts');
---
+++ server.ts
@@ -0,0 +1,39 @@
+console.log('start');
+
+import fs = require('fs');
+var express = require('express')
+
+var app = express();
+var static = '/media/simon/INTENSO4/Downloads';
+app.use('/static', function(req, res, next) {
+	var reqpath = decodeURIComponent(req.path);
+	var path = static + reqpath;
+	console.log(path);
+	fs.stat(path + '/', function(err, stats) {
+		if (err) {
+			//res.send(err)
+			next();
+		} else if (stats.isDirectory()) {
+			fs.readdir('/media/simon/INTENSO4/Downloads' + reqpath, function(err, files) {
+				var ret = files.map(
+					(x) => '<a href="/static' + req.path + '/' + encodeURIComponent(x) + '">' + x + '</a>'
+				).join('<br>\n');
+				res.send(ret);
+			});
+		} else
+			next();
+	});
+});
+app.use('/static', express.static('/media/simon/INTENSO4/Downloads'));
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+});
